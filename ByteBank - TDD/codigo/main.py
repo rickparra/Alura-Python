306627c2d31005a62fe940dcb40058a9b9abb1f9
GUSParra <hpbenitez7@gmail.com>
2023-04-19 16:16:33 -0300

    att alura python

---
+++ ByteBank - TDD/codigo/main.py
@@ -0,0 +1,7 @@
+from codigo.bytebank import Funcionario
+
+def teste_idade():
+    funcionario_teste = Funcionario('teste', '13/03/2000', 1111)
+    print(f'Teste = {funcionario_teste.idade()}')
+
+teste_idade()
---
+++ ByteBank - TDD/tests/test_bytebank.py
@@ -0,0 +1,32 @@
+from codigo.bytebank import Funcionario
+
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = '13/03/2000'
+        esperado = 23
+
+        funcionario_teste = Funcionario('teste', entrada, 1111)
+        resultado = funcionario_teste.idade()
+
+        assert resultado == esperado
+
+    def test_quando_sobrenome_recebe_Lucas_Carvalho_deve_retornar_Carvalho(self):
+        entrada = 'Lucas Carvalho'
+        esperado = 'Carvalho'
+
+        lucas = Funcionario(entrada, '11/11/2000', 1111)
+        resultado = lucas.sobrenome()
+
+        assert resultado == esperado
+
+    def test_quando_decrescimo_salario_recebe_100000_deve_retornar_900000(self):
+        entrada_salario = 100000
+        esperado = 90000
+        entrada_nome = 'Henrique Bragança'
+
+        funcionario_teste = Funcionario(entrada_nome, '11/11/2000', entrada_salario)
+        funcionario_teste.decrescimo_salario()
+        resultado = funcionario_teste.salario
+
+        assert resultado == esperado
+
